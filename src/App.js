81ef64cae865383257e31aa6c24eb166f4783b4b
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-06-13 01:27:57 -0700

    Added the basic editor with drag and drop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import DnDFlow from './DragNDrop/index';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{width: "100%", height: "90%"}}>
+      <DnDFlow/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LeZzzzarn React
         </a>
       </header>
     </div>
