636dc024a181be0271b40b3e6080737e578aa1f2
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-06-13 02:31:47 -0700

    Edges are now updateable

--- src/DragNDrop/index.tsx
+++ src/DragNDrop/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, DragEvent } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
+  updateEdge,
   removeElements,
   Controls,
   OnLoadParams,
@@ -30,9 +31,15 @@ const DnDFlow = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
   const [elements, setElements] = useState<Elements>(initialElements);
 
+  // gets called after end of edge gets dragged to another source or target
+  const onEdgeUpdate = (oldEdge: Edge, newConnection: Connection) =>
+    setElements((els) => updateEdge(oldEdge, newConnection, els));
   const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
   const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
   const onLoad = (_reactFlowInstance: OnLoadParams) => setReactFlowInstance(_reactFlowInstance);
+
+  const onEdgeUpdateStart = (_: React.MouseEvent, edge: Edge) => console.log('start update', edge);
+  const onEdgeUpdateEnd = (_: MouseEvent, edge: Edge) => console.log('end update', edge);
 
   const onDrop = (event: DragEvent) => {
     event.preventDefault();
@@ -59,6 +66,9 @@ const DnDFlow = () => {
             elements={elements}
             onConnect={onConnect}
             onElementsRemove={onElementsRemove}
+            onEdgeUpdateStart={onEdgeUpdateStart}
+            onEdgeUpdateEnd={onEdgeUpdateEnd}
+            onEdgeUpdate={onEdgeUpdate}
             onLoad={onLoad}
             onDrop={onDrop}
             onDragOver={onDragOver}
