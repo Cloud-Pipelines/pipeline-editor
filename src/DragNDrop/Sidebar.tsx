cd9599ee4dbdf400c6b59f84d7e2d68a1011cd9f
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-06-25 21:18:36 -0700

    Added a loaded pipeline component to sidebar

--- src/DragNDrop/Sidebar.tsx
+++ src/DragNDrop/Sidebar.tsx
@@ -1,4 +1,7 @@
-import React, { DragEvent } from 'react';
+import React, { DragEvent, useEffect, useState } from 'react';
+
+import {loadComponentFromUrl} from '../componentStore'
+import {ComponentSpec, TaskSpec} from '../componentSpec'
 
 const onDragStart = (event: DragEvent, nodeData: object) => {
   event.dataTransfer.setData('application/reactflow', JSON.stringify(nodeData));
@@ -6,6 +9,12 @@ const onDragStart = (event: DragEvent, nodeData: object) => {
 };
 
 const Sidebar = () => {
+  const [componentSpec, setComponentSpec] = useState<ComponentSpec | undefined>(undefined);
+
+  useEffect(() => {
+    loadComponentFromUrl("https://raw.githubusercontent.com/kubeflow/pipelines/603342c4b88fe2d69ff07682f702cd3601e883bb/components/PyTorch/Train_PyTorch_model/from_CSV/component.yaml").then(setComponentSpec);
+  }, []);
+
   return (
     <aside>
       <div className="description">You can drag these nodes to the pane on the right.</div>
@@ -32,6 +41,30 @@ const Sidebar = () => {
         }
       >
         Multi-port Node 3-2-1-1
+      </div>
+      <div className="react-flow__node react-flow__node-multihandle" draggable
+        onDragStart={(event: DragEvent) => {
+          // Using await inside onDragStart breaks event.dataTransfer.setData! 
+          //const componentSpec = await loadComponentFromUrl("https://raw.githubusercontent.com/kubeflow/pipelines/603342c4b88fe2d69ff07682f702cd3601e883bb/components/PyTorch/Train_PyTorch_model/from_CSV/component.yaml");
+          if (componentSpec === undefined) {
+            return;
+          }
+          const inputs = componentSpec.inputs ?? []
+          const inputNames = inputs.map(inputSpec => inputSpec.name);
+          const outputs = componentSpec.outputs ?? []
+          const outputNames = outputs.map(outputSpec => outputSpec.name);
+          const componentName = componentSpec.name ?? "Component"
+          console.log(inputNames)
+          return onDragStart(event, { multihandle: {
+            handles: {
+              top: { type: "target", ids: inputNames },
+              bottom: { type: "source", ids: outputNames },
+            },
+            label: componentName,
+          }});
+        }}
+      >
+        PyTorch/Train_PyTorch_model/from_CSV
       </div>
     </aside>
   );
