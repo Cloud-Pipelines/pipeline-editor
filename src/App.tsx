1dc6b42442c896af388d570b0d9e7b4a496677db
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-06-27 00:00:28 -0700

    Cleared the default App styles and page.
    The default style centers everything and I do not want that.
    We do not need the default header and logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import DnDFlow from './DragNDrop/index';
 
 function App() {
   return (
-    <div className="App" style={{width: "100%", height: "90%"}}>
+    <div className="App" style={{width: "100%", height: "100%"}}>
       <DnDFlow/>
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LeZzzzarn React
-        </a>
-      </header>
     </div>
   );
 }
