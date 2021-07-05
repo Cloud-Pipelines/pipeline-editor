d3533f66fc691a3229e1e9b4f99b645ddd3e762e
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-07-05 04:06:14 -0700

    Refactored everything to GraphComponentSpecFlow
    GraphComponentSpecFlow component wraps the ReactFlow component, encapsulating the GraphSpec rendering and modification. The ComponentSpec/GraphSpec is now the source of truth.
    This also fixes several issues like inputs connected to multiple upstreams.
    Graph inputs and outputs are not supported yet.

---
+++ src/DragNDrop/GraphComponentSpecFlow.tsx
@@ -0,0 +1,288 @@
+import React, { DragEvent, useState } from "react";
+import ReactFlow, {
+  ArrowHeadType,
+  Connection,
+  Edge,
+  ElementId,
+  Elements,
+  isEdge,
+  isNode,
+  Node,
+  OnLoadParams,
+  ReactFlowProps,
+  XYPosition,
+} from "react-flow-renderer";
+
+import {
+  ArgumentType,
+  GraphSpec,
+  TaskOutputArgument,
+  TaskSpec,
+} from "../componentSpec";
+import ComponentTaskNode from "./ComponentTaskNode";
+
+export interface GraphComponentSpecFlowProps
+  extends Omit<ReactFlowProps, "elements"> {
+  initialGraphSpec?: GraphSpec;
+}
+
+const nodeTypes = {
+  task: ComponentTaskNode,
+};
+
+const GraphComponentSpecFlow = ({
+  children,
+  //TODO:Replace GraphSpec with the full ComponentSpec
+  initialGraphSpec = { tasks: {} },
+  ...rest
+}: GraphComponentSpecFlowProps) => {
+  const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
+  const [originalGraphSpec, setGraphSpec] =
+    useState<GraphSpec>(initialGraphSpec);
+  let graphSpec = originalGraphSpec;
+
+  const nodes = Object.entries(graphSpec.tasks).map<Node<TaskSpec>>(
+    ([taskId, taskSpec]) => {
+      let position: XYPosition = { x: 0, y: 0 };
+      if (taskSpec.annotations !== undefined) {
+        try {
+          const layoutAnnotation = taskSpec.annotations[
+            "editor.position"
+          ] as string;
+          const decodedPosition = JSON.parse(layoutAnnotation);
+          position = { x: decodedPosition["x"], y: decodedPosition["y"] };
+        } catch (err) {}
+      }
+
+      return {
+        id: taskId,
+        data: taskSpec,
+        position: position,
+        type: "task",
+      };
+    }
+  );
+
+  const edges: Edge[] = Object.entries(graphSpec.tasks).flatMap(
+    ([taskId, taskSpec]) => {
+      return Object.entries(taskSpec.arguments ?? {}).flatMap(
+        ([inputName, argument]) => {
+          // TODO: Handle graph inputs
+          if (typeof argument !== "string" && "taskOutput" in argument) {
+            const taskOutput = argument.taskOutput;
+            const edge: Edge = {
+              id: `${taskOutput.taskId}_${taskOutput.outputName}-${taskId}_${inputName}`,
+              source: taskOutput.taskId,
+              sourceHandle: `output_${taskOutput.outputName}`,
+              target: taskId,
+              targetHandle: `input_${inputName}`,
+              arrowHeadType: ArrowHeadType.ArrowClosed,
+            };
+            return [edge];
+          } else {
+            return [];
+          }
+        }
+      );
+    }
+  );
+  // TODO: Handle graph outputs
+
+  const replaceGraphSpec = (newGraphSpec: GraphSpec) => {
+    graphSpec = newGraphSpec;
+    setGraphSpec(newGraphSpec);
+  };
+
+  const setTaskArgument = (
+    taskId: string,
+    inputName: string,
+    argument?: ArgumentType
+  ) => {
+    const oldTaskSpec = graphSpec.tasks[taskId];
+    const oldTaskSpecArguments = oldTaskSpec.arguments;
+    let newTaskSpecArguments: Record<string, ArgumentType> = {
+      ...oldTaskSpecArguments,
+    };
+    if (argument === undefined) {
+      delete newTaskSpecArguments[inputName];
+    } else {
+      newTaskSpecArguments[inputName] = argument;
+    }
+    let newGraphSpec: GraphSpec = {
+      ...graphSpec,
+      tasks: { ...graphSpec.tasks },
+    };
+    newGraphSpec.tasks[taskId] = {
+      ...oldTaskSpec,
+      arguments: newTaskSpecArguments,
+    };
+    replaceGraphSpec(newGraphSpec);
+  };
+
+  const removeTaskArgument = (taskId: string, inputName: string) =>
+    setTaskArgument(taskId, inputName, undefined);
+
+  const addConnection = (connection: Connection | Edge) => {
+    if (
+      connection.source === null ||
+      connection.sourceHandle === null ||
+      connection.sourceHandle === undefined ||
+      connection.target === null ||
+      connection.targetHandle === null ||
+      connection.targetHandle === undefined
+    ) {
+      return;
+    }
+    const inputName = connection.targetHandle.replace(/^input_/, "");
+    const outputName = connection.sourceHandle.replace(/^output_/, "");
+
+    const argument: TaskOutputArgument = {
+      taskOutput: {
+        taskId: connection.source,
+        outputName: outputName,
+      },
+    };
+
+    setTaskArgument(connection.target, inputName, argument);
+  };
+
+  const onConnect = (params: Connection | Edge) => {
+    addConnection(params);
+  };
+
+  const removeEdge = (edge: Edge) => {
+    // TODO: Handle graph input and output connections
+    if (
+      edge.sourceHandle === null ||
+      edge.sourceHandle === undefined ||
+      edge.targetHandle === null ||
+      edge.targetHandle === undefined
+    ) {
+      return;
+    }
+    const inputName = edge.targetHandle.replace(/^input_/, "");
+
+    removeTaskArgument(edge.target, inputName);
+  };
+
+  const removeNode = (node: Node) => {
+    const taskIdToRemove = node.id;
+
+    // Removing the outcoming edges
+    for (const [taskId, taskSpec] of Object.entries(graphSpec.tasks)) {
+      for (const [inputName, argument] of Object.entries(
+        taskSpec.arguments ?? {}
+      )) {
+        if (typeof argument !== "string" && "taskOutput" in argument) {
+          if (argument.taskOutput.taskId === taskIdToRemove) {
+            removeTaskArgument(taskId, inputName);
+          }
+        }
+      }
+    }
+
+    // ! TODO: Remove outcoming edges that go to graph outputs ? Delete the outputs themselves?
+
+    // Removing the task
+    let newGraphSpec: GraphSpec = {
+      ...graphSpec,
+      tasks: { ...graphSpec.tasks },
+    };
+    delete newGraphSpec.tasks[taskIdToRemove];
+    replaceGraphSpec(newGraphSpec);
+  };
+
+  const onElementsRemove = (elementsToRemove: Elements) => {
+    for (const element of elementsToRemove) {
+      if (isEdge(element)) {
+        removeEdge(element);
+      }
+    }
+    for (const element of elementsToRemove) {
+      if (isNode(element)) {
+        removeNode(element);
+      }
+    }
+  };
+
+  const onEdgeUpdate = (oldEdge: Edge, newConnection: Connection) => {
+    removeEdge(oldEdge);
+    addConnection(newConnection);
+  };
+
+  const onLoad = (_reactFlowInstance: OnLoadParams) =>
+    setReactFlowInstance(_reactFlowInstance);
+
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  };
+
+  const getId = (nodeType: string, nodeData: any): ElementId => {
+    const baseName: string = nodeData?.componentRef?.spec?.name ?? nodeType;
+    let finalName = baseName;
+    let index = 0;
+    const nodeNames = new Set(Object.keys(graphSpec.tasks));
+    while (nodeNames.has(finalName)) {
+      index++;
+      finalName = baseName + " " + index.toString();
+    }
+    return finalName;
+  };
+
+  const onDrop = (event: DragEvent) => {
+    event.preventDefault();
+
+    if (reactFlowInstance) {
+      const droppedData = event.dataTransfer.getData("application/reactflow");
+      if (droppedData === "") {
+        return;
+      }
+      const droppedDataObject = JSON.parse(droppedData);
+      const nodeType = Object.keys(droppedDataObject)[0];
+      const nodeData = droppedDataObject[nodeType];
+      const position = reactFlowInstance.project({
+        x: event.clientX,
+        y: event.clientY - 40,
+      });
+
+      if (nodeType === "task") {
+        let taskSpec = nodeData as TaskSpec;
+        let newAnnotations: Record<string, unknown> = {
+          ...taskSpec.annotations,
+        };
+        const taskPosition = { x: position.x, y: position.y };
+        newAnnotations["editor.position"] = JSON.stringify(taskPosition);
+        taskSpec.annotations = newAnnotations;
+        const taskId = getId(nodeType, nodeData);
+        graphSpec = { ...graphSpec, tasks: { ...graphSpec.tasks } };
+        graphSpec.tasks[taskId] = taskSpec;
+        replaceGraphSpec(graphSpec);
+      } else if (nodeType === "input") {
+        // TODO: Implement
+      } else if (nodeType === "output") {
+        // TODO: Implement
+      }
+    }
+  };
+
+  const elements = (nodes as Elements).concat(edges);
+
+  return (
+    <ReactFlow
+      {...rest}
+      elements={elements}
+      nodeTypes={nodeTypes}
+      onConnect={onConnect}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+      onEdgeUpdate={onEdgeUpdate}
+      onElementsRemove={onElementsRemove}
+      onLoad={onLoad}
+    >
+      {children}
+    </ReactFlow>
+  );
+};
+
+export default GraphComponentSpecFlow;
--- src/DragNDrop/index.tsx
+++ src/DragNDrop/index.tsx
@@ -1,108 +1,45 @@
-import React, { useState, DragEvent } from 'react';
-import ReactFlow, {
+import { useState } from 'react';
+import {
   ReactFlowProvider,
-  addEdge,
-  updateEdge,
-  removeElements,
   Controls,
-  OnLoadParams,
-  Elements,
-  Connection,
-  Edge,
-  ElementId,
-  Node,
   Background,
   MiniMap,
 } from 'react-flow-renderer';
 
+import { GraphSpec } from '../componentSpec';
+import GraphComponentSpecFlow from './GraphComponentSpecFlow';
 import Sidebar from './Sidebar';
-import ComponentTaskNode from './ComponentTaskNode';
+import { preloadComponentReferences, xgBoostQueryTrainPredictPipeline } from "./samplePipelines";
 
 import './dnd.css';
 
 const GRID_SIZE = 10;
 
-const nodeTypes = {
-  task: ComponentTaskNode,
-};
-
-const initialElements = [{ id: '1', type: 'input', data: { label: 'input node' }, position: { x: 250, y: 10 } }];
-
-const onDragOver = (event: DragEvent) => {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
-};
-
-let nodeNames = new Set<string>();
-const getId = (nodeType: string, nodeData: any): ElementId => {
-  const baseName = nodeData?.componentRef?.spec?.name ?? nodeType;
-  let finalName = baseName;
-  let index = 0;
-  while (nodeNames.has(finalName)) {
-    index++;
-    finalName = baseName + " " + index.toString();
-  }
-  nodeNames.add(finalName);
-  return finalName;
-};
-
 const DnDFlow = () => {
-  const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
-  const [elements, setElements] = useState<Elements>(initialElements);
+  const [graphSpec, setGraphSpec] = useState<GraphSpec | undefined>();
 
-  // gets called after end of edge gets dragged to another source or target
-  const onEdgeUpdate = (oldEdge: Edge, newConnection: Connection) =>
-    setElements((els) => updateEdge(oldEdge, newConnection, els));
-  const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
-  const onElementsRemove = (elementsToRemove: Elements) => {
-    for (const element of elementsToRemove) {
-      nodeNames.delete(element.id);
-    }
-    setElements((els) => removeElements(elementsToRemove, els));
-  };
-  const onLoad = (_reactFlowInstance: OnLoadParams) => setReactFlowInstance(_reactFlowInstance);
-
-  const onEdgeUpdateStart = (_: React.MouseEvent, edge: Edge) => console.log('start update', edge);
-  const onEdgeUpdateEnd = (_: MouseEvent, edge: Edge) => console.log('end update', edge);
-
-  const onDrop = (event: DragEvent) => {
-    event.preventDefault();
-
-    if (reactFlowInstance) {
-      const droppedData = event.dataTransfer.getData('application/reactflow');
-      if (droppedData === "") {
-        return;
-      }
-      const droppedDataObject = JSON.parse(droppedData);
-      const nodeType = Object.keys(droppedDataObject)[0];
-      const nodeData = droppedDataObject[nodeType];
-      const position = reactFlowInstance.project({ x: event.clientX, y: event.clientY - 40 });
-      const newNode: Node = {
-        id: getId(nodeType, nodeData),
-        type: nodeType,
-        position,
-        data: nodeData,
-      };
-
-      setElements((es) => es.concat(newNode));
+  if (graphSpec === undefined) {
+    try {
+      (async () => {
+        const pipeline = await preloadComponentReferences(xgBoostQueryTrainPredictPipeline);
+        if ("graph" in pipeline.implementation) {
+          setGraphSpec(pipeline.implementation.graph);
+        }
+      })();
+    } catch {
     }
   };
+
+  if (graphSpec === undefined) {
+    return (<></>);
+  }
 
   return (
     <div className="dndflow">
       <ReactFlowProvider>
         <div className="reactflow-wrapper">
-          <ReactFlow
-            elements={elements}
-            onConnect={onConnect}
-            onElementsRemove={onElementsRemove}
-            onEdgeUpdateStart={onEdgeUpdateStart}
-            onEdgeUpdateEnd={onEdgeUpdateEnd}
-            onEdgeUpdate={onEdgeUpdate}
-            onLoad={onLoad}
-            onDrop={onDrop}
-            onDragOver={onDragOver}
-            nodeTypes={nodeTypes}
+          <GraphComponentSpecFlow
+            initialGraphSpec={graphSpec}
             deleteKeyCode='Delete'
             multiSelectionKeyCode='Control'
             snapToGrid={true}
@@ -111,7 +48,7 @@ const DnDFlow = () => {
             <MiniMap/>
             <Controls />
             <Background gap={GRID_SIZE}/>
-          </ReactFlow>
+          </GraphComponentSpecFlow>
         </div>
         <Sidebar />
       </ReactFlowProvider>
