64b3fb981d2e12914cf1d408941b6fe6b2d4a9a2
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-07-12 15:20:08 -0700

    ComponentSpecAutoSaver - Added automatic save and load

--- src/DragNDrop/index.tsx
+++ src/DragNDrop/index.tsx
@@ -4,22 +4,73 @@ import {
   Controls,
   Background,
   MiniMap,
+  Node,
+  useStoreState,
 } from 'react-flow-renderer';
+import yaml from "js-yaml";
 
 import { ComponentSpec } from '../componentSpec';
-import GraphComponentSpecFlow from './GraphComponentSpecFlow';
+import GraphComponentSpecFlow, { augmentComponentSpec } from './GraphComponentSpecFlow';
 import Sidebar from './Sidebar';
 import { loadComponentFromUrl, XGBOOST_PIPELINE_URL } from "./samplePipelines";
 
 import './dnd.css';
 
 const GRID_SIZE = 10;
+const SAVED_COMPONENT_SPEC_KEY = "autosaved.component.yaml";
+
+const saveComponentSpec = (componentSpec: ComponentSpec, nodes?: Node[]) => {
+  try {
+    if (nodes !== undefined) {
+      componentSpec = augmentComponentSpec(componentSpec, nodes, true, true);
+    }
+    const componentText = yaml.dump(componentSpec, { lineWidth: 10000 });
+    window.sessionStorage.setItem(SAVED_COMPONENT_SPEC_KEY, componentText);
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+const loadComponentSpec = () => {
+  try {
+    const componentText = window.sessionStorage.getItem(SAVED_COMPONENT_SPEC_KEY);
+    if (componentText !== null) {
+      const loadedYaml = yaml.load(componentText);
+      if (loadedYaml !== null && typeof loadedYaml === "object") {
+        //TODO: Validate that the spec is valid
+        const savedComponentSpec = loadedYaml as ComponentSpec;
+        return savedComponentSpec;
+      }
+    }
+  } catch(err) {
+    console.error(err);
+  }
+  return undefined;
+}
+
+// Auto-saver is extracted to its own child component since useStoreState in the parent causes inifinite re-rendering
+// (each render of GraphComponentSpecFlow seems to change the Redux store).
+// This component seems to be triggered for every node movement, so even pure layout changes are saved.
+const ComponentSpecAutoSaver = ({
+  componentSpec,
+}: {
+  componentSpec: ComponentSpec;
+}) => {
+  const nodes = useStoreState((store) => store.nodes);
+  saveComponentSpec(componentSpec, nodes);
+  return null;
+};
 
 const DnDFlow = () => {
   const [componentSpec, setComponentSpec] = useState<ComponentSpec | undefined>();
 
   if (componentSpec === undefined) {
-    loadComponentFromUrl(XGBOOST_PIPELINE_URL).then(setComponentSpec);
+    const restoredComponentSpec = loadComponentSpec();
+    if (restoredComponentSpec === undefined) {
+      loadComponentFromUrl(XGBOOST_PIPELINE_URL).then(setComponentSpec);
+    } else {
+      setComponentSpec(restoredComponentSpec);
+    }
   };
 
   if (componentSpec === undefined) {
@@ -47,6 +98,7 @@ const DnDFlow = () => {
           componentSpec={componentSpec}
           setComponentSpec={setComponentSpec}
         />
+        <ComponentSpecAutoSaver componentSpec={componentSpec}/>
       </ReactFlowProvider>
     </div>
   );
