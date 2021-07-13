e531d7b0491961864fb38ac9dd1ebc29d2e432ca
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-07-13 15:32:22 -0700

    VertexAiExporter - Collapse by default

--- src/DragNDrop/VertexAiExporter.tsx
+++ src/DragNDrop/VertexAiExporter.tsx
@@ -25,7 +25,7 @@ const VertexAiExporter = ({pipelineName}: {pipelineName?: string}) => {
   const vertexPipelineSpecTextBlobUrl = URL.createObjectURL(vertexPipelineSpecTextBlob);
 
   return (
-    <details open>
+    <details>
       <summary>
         Cloud IR <a
           href={vertexPipelineSpecTextBlobUrl}
