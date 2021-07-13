7133f4ee7477996b2badb73d3799953cc485e44a
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-07-13 15:54:53 -0700

    GraphComponentLink - Small fixes
    Use the passed downloadFileName and linkText parameters.
    Return error instead of showing link leading to the error message.

--- src/DragNDrop/GraphComponentLink.tsx
+++ src/DragNDrop/GraphComponentLink.tsx
@@ -1,7 +1,6 @@
 import { useStoreState } from "react-flow-renderer";
 import yaml from "js-yaml";
 
-import { createGraphComponentSpecFromFlowElements } from "./graphComponentFromFlow";
 import { ComponentSpec } from "../componentSpec";
 import { augmentComponentSpec } from "./GraphComponentSpecFlow";
 
@@ -18,22 +17,21 @@ const GraphComponentLink = ({
 }: GraphComponentLinkProps) => {
   const nodes = useStoreState((store) => store.nodes);
 
-
   let componentText = "";
   try {
     componentSpec = augmentComponentSpec(componentSpec, nodes, false, true);
     componentText = yaml.dump(componentSpec, { lineWidth: 10000 });
   } catch (err) {
-    componentText = String(err);
+    return <>err.toString()</>;
   }
 
   const componentTextBlob = new Blob([componentText], { type: "text/yaml" }); // Or application/x-yaml (which leads to downloading)
   return (
     <a
       href={URL.createObjectURL(componentTextBlob)}
-      download={"component.yaml"}
+      download={downloadFileName}
     >
-      component.yaml
+      {linkText}
     </a>
   );
 };
