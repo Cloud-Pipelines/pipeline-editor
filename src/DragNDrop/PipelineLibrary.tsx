92b2acb9187b159a3dd4fecac9fe61333ba5b013
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-07-24 14:13:07 -0700

    PipelineLibrary - Addedt the pipeline library
    The pipeline library shows a list of the user pipelines.
    The user can import and export pipelines.
    Three pipelines are getting preloaded.

---
+++ src/DragNDrop/PipelineLibrary.tsx
@@ -0,0 +1,159 @@
+import { useCallback, useState, useEffect, useRef } from "react";
+import { ComponentSpec, isGraphImplementation } from "../componentSpec";
+import {
+  storeComponentFromUrl,
+  getAllComponentsFromList,
+  addComponentToListByText,
+  ComponentReferenceWithSpec,
+  loadComponentAsRefFromText,
+  resetComponentList,
+} from "../componentStore";
+import GraphComponentLink from "./GraphComponentLink";
+import {
+  preloadComponentReferences,
+  PRELOADED_PIPELINE_URLS,
+} from "./samplePipelines";
+
+const USER_PIPELINES_LIST_NAME = "user_pipelines";
+
+interface PipelineLibraryProps {
+  componentSpec?: ComponentSpec;
+  setComponentSpec?: (componentSpec: ComponentSpec) => void;
+}
+
+const PipelineLibrary = ({
+  componentSpec,
+  setComponentSpec,
+}: PipelineLibraryProps) => {
+  // const [errorMessage, setErrorMessage] = useState("");
+  const [componentRefs, setComponentRefs] = useState<
+    ComponentReferenceWithSpec[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      let componentRefs = await getAllComponentsFromList(
+        USER_PIPELINES_LIST_NAME
+      );
+      if (componentRefs.length === 0) {
+        // addComponentRefToList is prone to race conditions. Usually it ends up with only 2 pipelines.
+        // Plus there are race conditions that cause the last pipeline to be added twice.
+        // componentRefs = await Promise.all(
+        //   PRELOADED_PIPELINE_URLS.map(async (url) => {
+        //     const componentRef = await storeComponentFromUrl(url);
+        //     await preloadComponentReferences(componentRef.spec)
+        //     await addComponentRefToList(USER_PIPELINES_LIST_NAME, componentRef);
+        //     return componentRef;
+        //   })
+        // );
+        for (const url of PRELOADED_PIPELINE_URLS) {
+          const componentRef = await storeComponentFromUrl(url);
+          await preloadComponentReferences(componentRef.spec);
+          componentRefs.push(componentRef);
+        }
+        await resetComponentList(USER_PIPELINES_LIST_NAME, componentRefs);
+      }
+      setComponentRefs(componentRefs);
+    })();
+  }, []);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = async () => {
+        const binaryStr = reader.result;
+        if (binaryStr === null || binaryStr === undefined) {
+          console.error(`Dropped file reader result was ${binaryStr}`);
+          return;
+        }
+        try {
+          const componentRef1 = await loadComponentAsRefFromText(binaryStr);
+          if (!isGraphImplementation(componentRef1.spec.implementation)) {
+            console.error("Dropped component is not a graph component");
+            return;
+          }
+          // TODO: Do not load the component twice
+
+          const componentRef = await addComponentToListByText(
+            USER_PIPELINES_LIST_NAME,
+            binaryStr
+          );
+          console.debug("storeComponentText succeeded", componentRef);
+          // setErrorMessage("");
+          const allComponentRefs = await getAllComponentsFromList(
+            USER_PIPELINES_LIST_NAME
+          );
+          setComponentRefs(allComponentRefs);
+        } catch (err) {
+          // setErrorMessage(
+          //   `Error parsing the dropped file as component: ${err.toString()}.`
+          // );
+          console.error("Error parsing the dropped file as component", err);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const fileInput = useRef<HTMLInputElement>(null);
+  const componentLink = useRef<HTMLAnchorElement>(null);
+
+  return (
+    <div
+      style={{
+        //border: "1px solid black",
+        overflow: "auto",
+        whiteSpace: "nowrap",
+      }}
+    >
+      <input
+        ref={fileInput}
+        type="file"
+        accept=".yaml"
+        onChange={(e) => onDrop(Array.from(e.target.files ?? []))}
+        style={{ display: "none" }}
+      />
+      <button onClick={(e) => fileInput.current?.click()}>
+        + Import pipeline
+      </button>
+      <button
+        onClick={(e) => {
+          componentLink.current?.click();
+        }}
+      >
+        Export pipeline
+      </button>
+      {componentSpec && (
+        <GraphComponentLink
+          linkRef={componentLink}
+          componentSpec={componentSpec}
+          linkText="🔗"
+          downloadFileName={
+            (componentSpec.name ? componentSpec.name + "." : "") +
+            "pipeline.component.yaml"
+          }
+          style={{ textDecoration: "none" }}
+        />
+      )}
+      <div style={{ overflow: "auto" }}>
+        {componentRefs.map((componentRef) => (
+          <div key={componentRef.digest}>
+            ⋮ {/* ⋮ ≡ ⋅ */}
+            <button
+              className="link-button"
+              onClick={(e) => {
+                setComponentSpec?.(componentRef.spec);
+              }}
+            >
+              {componentRef.spec.name ?? "<Pipeline>"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PipelineLibrary;
