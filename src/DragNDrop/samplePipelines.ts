3b04ded3001f9d3f0ce9e60c43d817142228b48a
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-01-26 23:48:25 -0800

    feat(Sample pipelines): Added the Vertex AI AutoML Tables pipeline

--- src/DragNDrop/samplePipelines.ts
+++ src/DragNDrop/samplePipelines.ts
@@ -12,11 +12,13 @@ import { downloadComponentDataWithCache } from "../github";
 const TFX_PIPELINE_URL = "https://raw.githubusercontent.com/Ark-kun/pipelines/2765b13699ac28de523f499eeaa9eb2ed9b8798a/components/deprecated/tfx/_samples/TFX.pipeline.component.yaml"
 const XGBOOST_PIPELINE_URL = "https://raw.githubusercontent.com/Ark-kun/pipelines/2edfd25b5ee3a4aa149c24a225a50041fbd3662d/components/XGBoost/_samples/sample_pipeline.pipeline.component.yaml"
 const PYTORCH_PIPELINE_URL = "https://raw.githubusercontent.com/Ark-kun/pipelines/237cd6bc0b6db26f615c22897be20aad77270b50/components/PyTorch/_samples/Train_fully-connected_network.pipeline.component.yaml"
+const VERTEX_AI_AUTOML_TABLES_PIPELINE_URL = "https://raw.githubusercontent.com/Ark-kun/pipeline_components/44b0543525ab6149ce995a411f88997e7131a53d/components/google-cloud/Vertex_AI/AutoML/Tables/_samples/VertexAI.AutoML.Tables.pipeline.component.yaml"
 export const DATA_PASSING_PIPELINE_URL = "https://raw.githubusercontent.com/Ark-kun/pipelines/b45c82e42588ee0a86b8875d1908d972275bfd2f/samples/test/data_passing.pipeline.component.yaml"
 
 export const PRELOADED_PIPELINE_URLS = [
   XGBOOST_PIPELINE_URL,
   PYTORCH_PIPELINE_URL,
+  VERTEX_AI_AUTOML_TABLES_PIPELINE_URL,
   TFX_PIPELINE_URL,
 ];
 
